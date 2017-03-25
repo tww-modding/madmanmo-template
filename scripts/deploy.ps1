079a753d36fddfe2475b1cb5d83b7e92afccee11
Todd Bowles <todd_115@hotmail.com>
2017-03-25 16:21:52 +1000

    Incorporated better deployment script from harrying hounds

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -10,8 +10,8 @@ $VerbosePreference = "Continue";
 $here = Split-Path $script:MyInvocation.MyCommand.Path;
 $rootDirectoryPath = & "$here\_Find-RootDirectory.ps1" -SearchStart $here;
 
-$modFilePath = "$rootDirectoryPath\build\$version\packed";
-$destinationPath = "C:\Program Files (x86)\Steam\SteamApps\common\Total War WARHAMMER\data\$modFile";
+$sourcePath = "$rootDirectoryPath\build\$version\packed";
+$destinationPath = "C:\Program Files (x86)\Steam\SteamApps\common\Total War WARHAMMER\data";
 
 Write-Verbose "Deploying From [$modFilePath] to [$destinationPath]";
-$result = Copy-Item -Path $modFilePath -Destination $destinationPath -Force
+robocopy $sourcePath $destinationPath;
